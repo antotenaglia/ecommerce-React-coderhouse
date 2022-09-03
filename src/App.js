f238a317ff33b518ea5bb337d1d410eafeec4ba9
anto tenaglia <antonelatenaglia@gmail.com>
2022-09-03 11:20:33 -0300

    contador con botón

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import ItemCount from './components/ItemCount';
 
 function App() {
+  const stock = 5; 
+  const inicial = 1;
   return (
     <div className="App">
       <header className="App-header">
         <NavBar />
       </header>
-      <ItemListContainer greeting="¡BIENVENIDO!"/>
+      <ItemListContainer greeting= "¡BIENVENIDO!"/>
+      <ItemCount stock= {stock} inicial= {inicial}/>
       <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
---
+++ src/components/ItemCount.js
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+const ItemCount = ({stock, inicial}) => {
+    const [items, setItems] = useState (inicial)
+    const suma = () => items < stock ? setItems (items + 1) : console.log('No hay mas stock')
+    const resta = () => items > inicial ? setItems (items - 1) : console.log('No se admiten valores negativos')
+    return (
+        <div className="ItemCount-sumadorContenedor">
+            <div className="div">
+            <button onClick={suma} className="ItemCount-botonSuma">+</button>
+            <input type="text" readOnly value={items} className="ItemCount-valorItem"/>
+            <button onClick={resta} className="ItemCount-botonResta">-</button>
+            </div>
+        </div>
+    )
+}
+
+export default ItemCount
